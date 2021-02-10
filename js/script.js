d7f74944d7329f47118fcf1dc2bf1ba62233bdfa
Marco.seb <marco.seba96@gmail.com>
2021-02-10 16:34:45 +0100

    upgrade

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 //REPLICHIAMO NETFLIX
+var app = new Vue({
+    el:'#app',
+    data:{
+        apiKey:'e0b316189ed62d429752927965d22a18',
+        query:'',
+        lang:'it-IT'
+    },
+    methods:{
+        search(){
+            axios
+            .get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: this.apiKey,
+                    query: this.query,
+                    language: this.lang
+                }
+            })
+            .then((result) => {
+                console.log(result.data.results);
+            })
+            .catch((error) => alert('errori'));
+        }
+    }
+});
